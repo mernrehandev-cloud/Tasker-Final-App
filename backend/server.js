61e0e39aa71151d97de90876842c444d531be159
mernrehandev-cloud <wordpresshaachi@gmail.com>
2026-04-07 11:26:10 +0500

    ignore list updated and new route for ping added

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const connectToDatabase = require("./db");
-require('dotenv').config();
+require("dotenv").config();
 
 const app = express();
 
-const cors = require('cors');
+const cors = require("cors");
 
 app.use(cors());
 app.use(express.json());
@@ -13,15 +13,18 @@ app.use(express.urlencoded({ extended: true }));
 const host = process.env.HOST;
 const port = process.env.PORT;
 
-
 // app.use(express.static('public'));
-app.get('/', (req, res) => {
-    try {
-        res.status(200).json({ message: "Backend is Working", status: "okay" });
-    } catch (error) {
-        res.status(400).json({ message: "Backend is off", status: "off" });
-    }
-})
+app.get("/", (req, res) => {
+  try {
+    res.status(200).json({ message: "Backend is Working", status: "okay" });
+  } catch (error) {
+    res.status(400).json({ message: "Backend is off", status: "off" });
+  }
+});
+
+app.get("/ping", (req, res) => {
+  res.status(200).send("Server is Awake!");
+});
 
 app.use("/task_status", require("./routes/tasker_status.route"));
 app.use("/task_category", require("./routes/tasker_category.route"));
@@ -33,15 +36,15 @@ app.use("/login", require("./routes/login.route"));
 app.use("/signup", require("./routes/signup.route"));
 
 app.use((req, res, next) => {
-    res.status(404).json({ message: "Route not found" });
+  res.status(404).json({ message: "Route not found" });
 });
 
 connectToDatabase()
-    .then(() => {
-        app.listen(port, host, () => {
-            console.log(`Server is listening on http://${host}:${port}`);
-        });
-    })
-    .catch(error => {
-        console.error("Failed to run database, and Express:", error.message);
+  .then(() => {
+    app.listen(port, host, () => {
+      console.log(`Server is listening on http://${host}:${port}`);
     });
+  })
+  .catch((error) => {
+    console.error("Failed to run database, and Express:", error.message);
+  });
